9a904e510aff3ce1e254705b30ccf91e444f13cd
Adiya <frozen.at34@gmail.com>
2020-04-16 13:45:35 +0800

    inc, exp ugugdluudiig delgetsees tseverlelee

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var uiController = (function () {
     inComeLabel: ".budget__income--value",
     expeseLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
+    containerDiv: ".container",
   };
 
   return {
@@ -56,7 +57,10 @@ var uiController = (function () {
           tusuv.huvi;
       }
     },
-
+    deleleListItem: function (id) {
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
     addListItem: function (item, type) {
       // Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ
 
@@ -64,11 +68,11 @@ var uiController = (function () {
       if (type === "inc") {
         list = DOMstrings.inComeList;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><iclass="ion-ios-close-outline"></iclass="ion-ios-close-outline"></button></div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else {
         list = DOMstrings.exPense;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       // Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглаж өөрчилж өгнө
       html = html.replace("%id%", item.id);
@@ -122,6 +126,15 @@ var financeController = (function () {
       data.tusuv = data.totals.inc - data.totals.exp;
       // Орлого зарлагын хувийг тооцоолно
       data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map(function (el) {
+        return el.id;
+      });
+      var index = ids.indexOf(id);
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
     },
     tusuviigAvah: function () {
       return {
@@ -193,6 +206,25 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function (event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+          console.log(type + " ===> " + itemId);
+
+          // Санхүүгийн модулиас type, id ашиглаад устгана
+          financeController.deleteItem(type, itemId);
+
+          // Дэлгэц дээрээс энэ элементийг устгана
+          uiController.deleleListItem(id);
+          // Үлдэгдэл тооцоог шинэчилж харуулна
+        }
+      });
   };
 
   return {
